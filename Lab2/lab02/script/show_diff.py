c1701b4411849dbdec49061c09f558f7a426bca3
Rui Wang <1102131860@qq.com>
2025-02-21 01:31:40 -0500

    Add a script to show the difference between output and refered

---
+++ Lab2/lab02/script/show_diff.py
@@ -0,0 +1,12 @@
+output = "../run/output.txt"
+ref_output = "../run/output_ref.txt"
+
+count = 0
+with open(output, "r", encoding="utf-8") as output_file, open(ref_output, "r", encoding="utf-8") as ref_output_file:
+    for i, (line_out, line_ref) in enumerate(zip(output_file, ref_output_file)):
+        if line_out != line_ref:
+            print(f"[+] line {i}: {line_out.strip()}")
+            print(f"[-] line {i}: {line_ref.strip()}")
+            count += 1
+
+print(f"Found {count} differences between two files")
